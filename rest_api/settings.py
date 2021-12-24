3e14e511e02e9fe861069e052d577abe3ff87f4a
enuii3 <b.nxxxi551@gmail.com>
2021-12-25 08:35:12 +0900

    herokuUrlの追加

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -28,7 +28,7 @@ DEBUG = config('DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['next-api-lesson.herokuapp.com']
 
 
 # Application definition
